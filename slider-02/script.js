54dcd83cbe202091a6e53187de7845a0759e108e
ash <awebdeff@gmail.com>
2020-03-08 15:20:01 +0300

    done

--- slider-02/script.js
+++ slider-02/script.js
@@ -3,6 +3,7 @@ const dotsArea = document.getElementsByClassName('dots-block')[0]
 const slides = document.getElementsByClassName('slides-item')
 const prevBtn = document.getElementById('left-button')
 const nextBtn = document.getElementById('right-button')
+
 slideIndex = 1
 
 showSlides(slideIndex)
@@ -26,23 +27,15 @@ function showSlides(n) {
   dots[slideIndex - 1].classList.add('active')
 }
 
-function plusSlides(n) {
-  showSlides(slideIndex += n)
-}
+function plusSlides(n) { showSlides(slideIndex += n) }
 
-function currentSlides(n) {
-  showSlides(slideIndex = n)
-}
+function currentSlides(n) { showSlides(slideIndex = n) }
 
-prevBtn.onclick = function() {
-  plusSlides(-1)
-}
+prevBtn.onclick = function () { plusSlides(-1) }
 
-nextBtn.onclick = function() {
-  plusSlides(1)
-}
+nextBtn.onclick = function () { plusSlides(1) }
 
-dotsArea.onclick = function(e) {
+dotsArea.onclick = function (e) {
   for (let i = 0; i < dots.length + 1; i++) {
     if (e.target.classList.contains('dots-item') && e.target == dots[i - 1]) {
       currentSlides(i)
