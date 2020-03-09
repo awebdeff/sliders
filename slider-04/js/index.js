ba1225508402522bc920be53a1a8463d40934b90
ash <awebdeff@gmail.com>
2020-03-09 16:11:50 +0300

    initial commit

---
+++ slider-04/js/index.js
@@ -0,0 +1,24 @@
+const images = document.querySelectorAll('.infinity-slider img')
+let current = 0
+
+function slider() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.add('opacity0')
+  }
+  images[current].classList.remove('opacity0')  
+}
+
+slider()
+
+document.querySelector('.btn-ifinity-up').onclick = function() {
+  if (current - 1 == -1) {
+    current = images.length - 1
+  } else { current-- }
+  slider()
+}
+document.querySelector('.btn-ifinity-down').onclick = function() {
+  if (current + 1 == images.length) {
+    current = 0
+  } else { current++ }
+  slider()
+}
